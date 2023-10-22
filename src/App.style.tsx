f1d1998b3bb3233181c7aaa060469c1f55390c2e
stat1202 <hseungjae1202@naver.com>
2023-10-22 17:31:19 +0900

    Feat : BoilerPlate 수정
    color 추가
    font 추가
    App.tsx 초기 화면 구성 추가
    reset css 추가

---
+++ src/App.style.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Layout = styled.div`
+  display: flex;
+  width: 100%;
+  min-height: 100vh;
+  justify-content: center;
+  background-color: antiquewhite;
+`;
+export const AppWrapper = styled.div`
+  width: 100%;
+  max-width: 600px;
+  background-color: white;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import "./App.css";
 import New from "./pages/New/New";
 import { Route, Routes } from "react-router";
 import UISample from "./pages/UISample/UISample";
 import IconTest from "./pages/IconTest/IconTest";
+import * as S from "./App.style";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<New />}></Route>
-      <Route path="/ui-sample" element={<UISample />}></Route>
-      <Route path="/icon-test" element={<IconTest />}></Route>
-    </Routes>
+    <S.Layout>
+      <S.AppWrapper>
+        <Routes>
+          <Route path="/" element={<New />}></Route>
+          <Route path="/ui-sample" element={<UISample />}></Route>
+          <Route path="/icon-test" element={<IconTest />}></Route>
+        </Routes>
+      </S.AppWrapper>
+    </S.Layout>
   );
 }
 
