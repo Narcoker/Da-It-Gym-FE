827d0d300816bf1df783a113d3bf366b0ddfbc05
stat1202 <hseungjae1202@naver.com>
2023-10-26 10:16:33 +0900

    Docs : Routines.style.tsx 삭제

--- src/pages/MyPage/components/Routines/Routines.tsx
+++ src/pages/MyPage/components/Routines/Routines.tsx
@@ -2,13 +2,15 @@ import RoutineUser from "../../../../components/RoutineUser/RoutineUser";
 
 export default function Routines() {
   return (
-    <RoutineUser
-      src=""
-      userName="user-nick"
-      info="자기소개 입니다"
-      likeCount="20"
-      shareCount="20"
-      timeAgo="4시간 전"
-    />
+    <>
+      <RoutineUser
+        src=""
+        userName="user-nick"
+        info="자기소개 입니다"
+        likeCount="20"
+        shareCount="20"
+        timeAgo="4시간 전"
+      />
+    </>
   );
 }
