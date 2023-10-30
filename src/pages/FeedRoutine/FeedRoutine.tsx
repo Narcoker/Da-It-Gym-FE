cd3048614326b0d7af7a077842bca224865f8081
Narcoker <jykim9868@gmail.com>
2023-10-30 17:28:23 +0900

    Feat : DIG-14 루틴 목록 페이지 UI 완료
    분할법 필터링, 탭, 루틴 목록 출력 UI 구현 완료

--- src/pages/FeedRoutine/FeedRoutine.tsx
+++ src/pages/FeedRoutine/FeedRoutine.tsx
@@ -1,29 +1,122 @@
-import ExercisePartLabel from "../../components/ExercisePartLabel/ExercisePartLabel";
+import { useState } from "react";
+import HashTagButton from "../../components/HashtagButton/HashtagButton";
 import Nav from "../../components/Nav/Nav";
+import RoutineUser from "../../components/RoutineUser/RoutineUser";
 import * as S from "./FeedRoutine.style";
 
+const tempData = [
+  {
+    userImg:
+      "https://images.chosun.com/resizer/lGyzt5Hi0efXfaeVhy5gXwXHilc=/616x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/52PNRX6QMNCRDD3QBAFB6XJJ6M.jpg",
+    userName: "가슴왕 재규니",
+    info: "안녕하세요 가슴을 조지는 루틴입니다. ",
+    likeCount: 1000,
+    shareCount: 100000,
+    createdAt: new Date(),
+  },
+  {
+    userImg:
+      "https://images.chosun.com/resizer/lGyzt5Hi0efXfaeVhy5gXwXHilc=/616x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/52PNRX6QMNCRDD3QBAFB6XJJ6M.jpg",
+    userName: "가슴왕 재규니",
+    info: "안녕하세요 가슴을 조지는 루틴입니다. ",
+    likeCount: 1000,
+    shareCount: 100000,
+    createdAt: new Date(),
+  },
+  {
+    userImg:
+      "https://images.chosun.com/resizer/lGyzt5Hi0efXfaeVhy5gXwXHilc=/616x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/52PNRX6QMNCRDD3QBAFB6XJJ6M.jpg",
+    userName: "가슴왕 재규니",
+    info: "안녕하세요 가슴을 조지는 루틴입니다. ",
+    likeCount: 1000,
+    shareCount: 100000,
+    createdAt: new Date(),
+  },
+  {
+    userImg:
+      "https://images.chosun.com/resizer/lGyzt5Hi0efXfaeVhy5gXwXHilc=/616x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/52PNRX6QMNCRDD3QBAFB6XJJ6M.jpg",
+    userName: "가슴왕 재규니",
+    info: "안녕하세요 가슴을 조지는 루틴입니다. ",
+    likeCount: 1000,
+    shareCount: 100000,
+    createdAt: new Date(),
+  },
+  {
+    userImg:
+      "https://images.chosun.com/resizer/lGyzt5Hi0efXfaeVhy5gXwXHilc=/616x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/52PNRX6QMNCRDD3QBAFB6XJJ6M.jpg",
+    userName: "가슴왕 재규니",
+    info: "안녕하세요. 오늘도 역시 가슴을 조지는 루틴입니다.abcdefghi",
+    likeCount: 1000,
+    shareCount: 100000,
+    createdAt: new Date(),
+  },
+];
+
+export type SelectedDivision = "무분할" | "2분할" | "3분할" | "4분할" | "5분할" | "6분할";
+export type SelectedTab = "전체 보기" | "팔로우 보기" | "추천";
+
 export default function FeedRoutine() {
+  const [selectedTab, setSelectedTab] = useState<SelectedTab>("전체 보기");
+  const handleSelectedTab = (tab: SelectedTab) => {
+    setSelectedTab(tab);
+  };
+
   return (
     <>
-      <Nav type="top" />
-      <S.ExercisePartLabels>
-        <ExercisePartLabel name="무분할" exercisePart="back" type="exercise" />
-        <ExercisePartLabel name="2분할" exercisePart="back" type="exercise" />
-        <ExercisePartLabel name="3분할" exercisePart="back" type="exercise" />
-        <ExercisePartLabel name="4분할" exercisePart="back" type="exercise" />
-        <ExercisePartLabel name="5분할" exercisePart="back" type="exercise" />
-        <ExercisePartLabel name="6분할+" exercisePart="back" type="exercise" />
-      </S.ExercisePartLabels>
+      <S.Header>
+        <Nav type="top" />
 
-      <S.Tabs>
-        <S.Tab>전체 보기</S.Tab>
-        <S.Tab>팔로우 보기</S.Tab>
-        <S.Tab>추천</S.Tab>
-      </S.Tabs>
+        <S.ExercisePartLabels>
+          <HashTagButton label="무분할" type="division" />
+          <HashTagButton label="2분할" type="division" />
+          <HashTagButton label="3분할" type="division" />
+          <HashTagButton label="4분할" type="division" />
+          <HashTagButton label="5분할" type="division" />
+          <HashTagButton label="6분할+" type="division" />
+        </S.ExercisePartLabels>
+
+        <S.Tabs>
+          <S.Tab
+            isSelected={selectedTab === "전체 보기"}
+            onClick={() => {
+              handleSelectedTab("전체 보기");
+            }}
+          >
+            전체 보기
+          </S.Tab>
+          <S.Tab
+            isSelected={selectedTab === "팔로우 보기"}
+            onClick={() => {
+              handleSelectedTab("팔로우 보기");
+            }}
+          >
+            팔로우 보기
+          </S.Tab>
+          <S.Tab
+            isSelected={selectedTab === "추천"}
+            onClick={() => {
+              handleSelectedTab("추천");
+            }}
+          >
+            추천
+          </S.Tab>
+        </S.Tabs>
+      </S.Header>
 
       <S.Routines>
-        루틴 목록
+        {tempData.map((routine) => (
+          <RoutineUser
+            src={routine.userImg}
+            userName={routine.userName}
+            info={routine.info}
+            likeCount={routine.likeCount.toString()}
+            shareCount={routine.shareCount.toString()}
+            timeAgo={routine.createdAt.toDateString()}
+          />
+        ))}
       </S.Routines>
+
+      <Nav type="home" />
     </>
   );
 }
