e424ecd267a963e6abbd31b40e3dee385da326dd
JamesJoe0830 <kyeun950830@gmail.com>
2023-11-23 22:58:54 +0900

    Fix : DIG-162 피드 일지 수정 작업

--- src/pages/FeedDiary/FeedDiary.tsx
+++ src/pages/FeedDiary/FeedDiary.tsx
@@ -1,17 +1,12 @@
-import { useSearchParams } from "react-router-dom";
 import * as S from "./FeedDiary.style";
 import * as Icon from "../../components/Icon";
 import FeedDiaryHeader from "./components/FeedDiaryHeader";
 import FeedDiaryBody from "./components/FeedDiaryBody";
-import Nav from "../../components/Nav/Nav";
 import SearchTag from "./components/SearchTag";
 import { useRef } from "react";
 
 export default function FeedDiary() {
-  const [searchParams] = useSearchParams();
   const topRef = useRef<HTMLDivElement>(null);
-  const query = searchParams.get("section");
-  console.log("쿼리", query);
 
   // scrollTop
   const scrollToTop = () => {
